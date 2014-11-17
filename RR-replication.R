3b844ad9346e40db089dba332e4149f34291ca28
steve <steven.v.miller@gmail.com>
2014-11-17 17:02:12 -0500

    Update RR-replication.R

--- RR-replication.R
+++ RR-replication.R
@@ -5,3 +5,20 @@ data <- getURL("https://raw.githubusercontent.com/svmiller/reinhart-rogoff/maste
 Data <- read.csv(text = data)
 
 summary(Data)
+
+Data$dgcat <- NA
+Data$dgcat[Data$debtgdp > 0 & Data$debtgdp < 30] <- "0-30%"
+Data$dgcat[Data$debtgdp >= 30 & Data$debtgdp < 60] <- "30-60%"
+Data$dgcat[Data$debtgdp >= 60 & Data$debtgdp < 90] <- "60-90%"
+Data$dgcat[Data$debtgdp >= 90] <- "90% and above"
+
+summary(M1 <- lm(dRGDP ~ factor(dgcat), data=Data))
+
+Data$dgcat2 <- NA
+Data$dgcat2[Data$debtgdp > 0 & Data$debtgdp < 30] <- "0-30%"
+Data$dgcat2[Data$debtgdp >= 30 & Data$debtgdp < 60] <- "30-60%"
+Data$dgcat2[Data$debtgdp >= 60 & Data$debtgdp < 90] <- "60-90%"
+Data$dgcat2[Data$debtgdp >= 90 & Data$debtgdp < 120] <- "90-120%"
+Data$dgcat2[Data$debtgdp >= 120] <- "Above 120%"
+
+summary(M2 <- lm(dRGDP ~ factor(dgcat2), data=Data))
