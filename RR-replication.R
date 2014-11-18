ce9a3e32072c1575dc34726a2353cabc034b6784
steve <steven.v.miller@gmail.com>
2014-11-17 19:01:38 -0500

    Update RR-replication.R

--- RR-replication.R
+++ RR-replication.R
@@ -22,3 +22,19 @@ Data$dgcat2[Data$debtgdp >= 90 & Data$debtgdp < 120] <- "90-120%"
 Data$dgcat2[Data$debtgdp >= 120] <- "Above 120%"
 
 summary(M2 <- lm(dRGDP ~ factor(dgcat2), data=Data))
+
+Data$dg0030 <- 0
+Data$dg0030[Data$debtgdp > 0 & Data$debtgdp < 30] <- 1
+Data$dg3060 <- 0
+Data$dg3060[Data$debtgdp >= 30 & Data$debtgdp < 60] <- 1
+Data$dg6090 <- 0
+Data$dg6090[Data$debtgdp >= 60 & Data$debtgdp < 90] <- 1
+Data$dg90plus <- 0
+Data$dg90plus[Data$debtgdp >= 90] <- 1
+
+Data$dgcat <- as.factor(Data$dgcat)
+summary(Z1 <- zelig(dRGDP ~ dgcat, data = Data, model="ls"))
+
+Z1.low <- setx(Z1, dgcat  = "90% and above")
+Z1.sim <-sim(Z1, x = Z1.low)
+summary(Z1.sim)
