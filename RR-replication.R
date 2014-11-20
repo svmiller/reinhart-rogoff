37a5a276111577625698d10957ffc6b5095557d3
steve <steven.v.miller@gmail.com>
2014-11-19 19:26:44 -0500

    Update RR-replication.R

--- RR-replication.R
+++ RR-replication.R
@@ -31,6 +31,33 @@ Data$dgcat <- as.factor(Data$dgcat)
 summary(Z1 <- zelig(dRGDP ~ dgcat, data = Data, model="ls"))
 
 Z1.low <- setx(Z1, dgcat  = "90% and above")
-Z1.sim <-sim(Z1, x = Z1.low)
-summary(Z1.sim)
+summary(Z1.sim <-sim(Z1, x = Z1.low))
 
+## Compare expected values of Y from Z1.sim with the means.
+## The expected value of Y we got makes sense with the means, but not what RR reported.
+
+(RR.correct.mean <- with(Data, tapply( dRGDP, dgcat, mean, na.rm=TRUE )))
+RR.correct.mean.df <- data.frame(RR.correct.mean, dgcat=names(RR.correct.mean) )
+
+## Let's start retracing RR's steps, starting first with equal weight means.
+
+(RR.equalwt.mean <- with(Data, tapply( dRGDP, list(Country,dgcat), mean, na.rm=TRUE )))
+
+## Recall that RR chose to omit Aussieland, New Zealand, and Canada for the first few years after WWII.
+## These observations are inconsistent with their findings. They chose to omit it (despite including the US in these years).
+
+
+RR.selective <- subset(Data,
+                       !((Year<1950 & Country=="New Zealand") | (Year<1951 & Country=="Australia") | (Year<1951 & Country=="Canada") ))
+(RR.selective.mean <- with(RR.selective, tapply( dRGDP, list(Country,dgcat), mean, na.rm=TRUE )))
+
+with(RR.selective, tapply( dRGDP, dgcat, mean, na.rm=TRUE ))
+
+
+RR.selective.spreadsheet <- subset(RR.selective, ! Country %in% c("Australia","Austria","Belgium","Canada","Denmark") )
+RR.selective.spreadsheet.transcription <- with(RR.selective.spreadsheet, tapply( dRGDP, list(Country,dgcat), mean, na.rm=TRUE ))
+
+RR.selective.spreadsheet.transcription["New Zealand",4] <- -7.9
+
+(RR.published.mean <- apply(RR.selective.spreadsheet.transcription,2,mean,na.rm=TRUE))
+RR.published.mean.df <- data.frame(RR.published.mean , dgcat=names(RR.published.mean) )
